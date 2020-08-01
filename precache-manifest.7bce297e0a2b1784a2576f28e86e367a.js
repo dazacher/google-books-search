7823bdd2d4ded99e6c55c9014aa86841ad65d011
Dazacher <dazacher2000@yahoo.com>
2020-08-01 07:03:37 -0700

    Updates

---
+++ precache-manifest.7bce297e0a2b1784a2576f28e86e367a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e20a3641966df47ca58205bff3e902",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "e14ebf1c65520b0445e5",
+    "url": "/google-book-search/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "60a42514e841cc254139",
+    "url": "/google-book-search/static/css/main.bd3d9fc9.chunk.css"
+  },
+  {
+    "revision": "e14ebf1c65520b0445e5",
+    "url": "/google-book-search/static/js/2.8cd986ff.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/google-book-search/static/js/2.8cd986ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a42514e841cc254139",
+    "url": "/google-book-search/static/js/main.2108f75e.chunk.js"
+  },
+  {
+    "revision": "b84b284927e949143af8",
+    "url": "/google-book-search/static/js/runtime-main.60e61126.js"
+  }
+]);
